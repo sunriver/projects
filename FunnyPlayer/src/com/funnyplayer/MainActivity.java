dde95e5cd62930edf7d5f89ec885437cb52b76bd
leon <leon@ubuntu.(none)>
2013-03-28 14:33:43 +0800

    add notification on the top bar

--- FunnyPlayer/src/com/funnyplayer/MainActivity.java
+++ FunnyPlayer/src/com/funnyplayer/MainActivity.java
@@ -1,12 +1,18 @@
 package com.funnyplayer;
 
 
+import java.util.Random;
+
 import android.media.AsyncPlayer;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.ImageView;
@@ -14,6 +20,7 @@ import android.view.View;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 	private static final String TAG = "AsyncPlayer";
+	private static final int NOTIFICATION_ID = 133948384;
 
 	private AsyncPlayer mAsyncPlayer;
 	private ImageView mPreviousImg;
@@ -32,6 +39,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mAsyncPlayer = new AsyncPlayer(TAG);
 		initListener();
 		play();
+		showNotification();
 	}
 	
 
@@ -83,5 +91,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
 				+ "/test.mp3"), false, AudioManager.STREAM_MUSIC);
 	}
 
+	
+	private void showNotification() {
+		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		Notification notification = new Notification(R.drawable.ic_launcher, "FunnyPlayer", System.currentTimeMillis());
+		notification.defaults = Notification.DEFAULT_ALL;
+
+		PendingIntent pendintIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+		String title = getString(R.string.app_name);
+		notification.setLatestEventInfo(this, title, title, pendintIntent);
+		nm.notify(NOTIFICATION_ID, notification);
+	}
 
 }
