2ce5e4dee652718e9753b668972c2be3cf00ea8b
sunriver <lu_0047@163.com>
2013-05-01 23:56:38 -0700

    add tab layout

--- android/FunnyPlayer/src/com/funnyplayer/MainActivity.java
+++ android/FunnyPlayer/src/com/funnyplayer/MainActivity.java
@@ -1,6 +1,5 @@
 package com.funnyplayer;
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +29,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.VideoView;
@@ -37,129 +37,80 @@ import android.view.View;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 	private static final String TAG = "AsyncPlayer";
-	
+
 	private static final int NOTIFICATION_ID = 133948384;
-	
-	
-	private File  mMusicDir;
-	private AsyncPlayer mAsyncPlayer;
-	private ImageView mPreviousImg;
-	private ImageView mNextImg;
-	private ImageView mStartOrStopImg;
-	private ListView  mMusicListView;
-	private ArrayAdapter<String> mAdapter;
-	private boolean isPlaying;
-	private int mCurrentPosition = 0;
-	private DownLoadThread mDownLoadThread;
-	private VideoView mVideoView;
-	
+
+	private File mMusicDir;
+
 	private FolderTab mFolderTab;
 	private PlaylistTab mPlaylistTab;
-	
+
+	private ImageButton mFolderIndicator;
+	private ImageButton mPlaylistIndicator;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+		mFolderIndicator = (ImageButton) findViewById(R.id.tab_indicator_folder);
+		mPlaylistIndicator = (ImageButton) findViewById(R.id.tab_indicator_playlist);
 		mFolderTab = new FolderTab(findViewById(R.id.tab_folder));
 		mPlaylistTab = new PlaylistTab(findViewById(R.id.tab_playlist));
-
-	}
-	
-	private void init() {
-		initListener();
-		initAdapter();
-//		showNotification();
-	}
-	
-	private void initAdapter() {
-		for (String fileName : mMusicDir.list()) {
-			if (fileName.endsWith(".mp3")) {
-				mAdapter.add(fileName);
-			}
-		}
-		mAdapter.notifyDataSetChanged();
-	}
-
-
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-	}
-	
-	
-    @Override
-    protected void onPause() {
-    	super.onPause();
-    }
-
-	private void initListener() {
-		mPreviousImg.setOnClickListener(this);
-		mNextImg.setOnClickListener(this);
-		mStartOrStopImg.setOnClickListener(this);
 		
-		mMusicListView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				mCurrentPosition = position;
-				play();
-			}
-			
-		});
+		init();
 	}
 
+	private void init() {
+		mFolderIndicator.setOnClickListener(this);
+		mPlaylistIndicator.setOnClickListener(this);
+	}
 
 	@Override
 	public void onClick(View v) {
-		
+		switch (v.getId()) {
+		case R.id.tab_indicator_folder:
+			mFolderTab.show();
+			mPlaylistTab.hide();
+			break;
+		case R.id.tab_indicator_playlist:
+			mPlaylistTab.show();
+			mFolderTab.hide();
+			break;
+		}
 	}
-	
+
 	private void playFromUrl() {
-		String url = "http://down.51voa.com/201303/0_k9ewwln6_0_2euw79ko.mp3";
-		Uri uri = Uri.parse(url);
-		mAsyncPlayer.play(getApplication(), uri,  false, AudioManager.STREAM_MUSIC);
-		
-//		mDownLoadThread.add(url, new HttpCallback() {
-//			@Override
-//			public int callback(String fileName, long totalSize, long progress) {
-//				Log.v(TAG, "MainActivity::HttpCallback(): totalSize = " + totalSize + "  progress:" + progress);
-//				Uri uri = Uri.parse(fileName);
-//				mAsyncPlayer.play(getApplication(), uri,  false, AudioManager.STREAM_MUSIC);
-//				return 0;
-//			}});
+		// String url =
+		// "http://down.51voa.com/201303/0_k9ewwln6_0_2euw79ko.mp3";
+		// Uri uri = Uri.parse(url);
+		// mAsyncPlayer.play(getApplication(), uri, false,
+		// AudioManager.STREAM_MUSIC);
+
+		// mDownLoadThread.add(url, new HttpCallback() {
+		// @Override
+		// public int callback(String fileName, long totalSize, long progress) {
+		// Log.v(TAG, "MainActivity::HttpCallback(): totalSize = " + totalSize +
+		// "  progress:" + progress);
+		// Uri uri = Uri.parse(fileName);
+		// mAsyncPlayer.play(getApplication(), uri, false,
+		// AudioManager.STREAM_MUSIC);
+		// return 0;
+		// }});
 	}
 
-	private void previous() {
-		mCurrentPosition = Math.max(0, mCurrentPosition - 1);
-		play();
-	}
-
-	private void next() {
-		mCurrentPosition = Math.min(mAdapter.getCount(), mCurrentPosition + 1);
-		play();
-	}
-
-	private void play() {
-		isPlaying = true;
-		String fileName = "/" + mAdapter.getItem(mCurrentPosition);
-		Uri uri = Uri.parse("file://"+ mMusicDir + fileName);
-		mAsyncPlayer.play(getApplication(), uri,  false, AudioManager.STREAM_MUSIC);
-	}
-	
 	private void showNotification() {
 		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-		Notification notification = new Notification(R.drawable.ic_launcher, "FunnyPlayer", System.currentTimeMillis());
+		Notification notification = new Notification(R.drawable.ic_launcher,
+				"FunnyPlayer", System.currentTimeMillis());
 		notification.defaults = Notification.DEFAULT_ALL;
 
-		PendingIntent pendintIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+		PendingIntent pendintIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, MainActivity.class),
+				PendingIntent.FLAG_UPDATE_CURRENT);
 		String title = getString(R.string.app_name);
 		notification.setLatestEventInfo(this, title, title, pendintIntent);
 		nm.notify(NOTIFICATION_ID, notification);
 	}
-
-
 
 }
