7470837adddaaecc26e878684054d1828d06164c
sunriver <lu_0047@163.com>
2013-08-23 03:54:59 -0700

    FixBug : APK crash if new call

--- android/FunnyPlayer/src/com/funnyplayer/broadcast/PhoneStatReceiver.java
+++ android/FunnyPlayer/src/com/funnyplayer/broadcast/PhoneStatReceiver.java
@@ -11,41 +11,38 @@ import android.util.Log;
 
 public class PhoneStatReceiver extends BroadcastReceiver {
 
-	private static final String TAG = "PhoneStatReceiver";
-
-	private static boolean incomingFlag = false;
-
-	private static String incoming_number = null;
+	private static final String TAG = PhoneStatReceiver.class.getSimpleName();
+	private MusicState mLastState = MusicState.PAUSED;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
-			incomingFlag = false;
-			String phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-			Log.i(TAG, "call OUT:" + phoneNumber);
-			MusicUtil.pause(context);
+			if (MusicUtil.isPlaying()) {
+				mLastState = MusicState.PLAYING;
+				MusicUtil.pause(context);
+			}
 		} else {
 			TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
 			switch (tm.getCallState()) {
 			case TelephonyManager.CALL_STATE_RINGING:
-				MusicUtil.pause(context);
-				incomingFlag = true;
-				incoming_number = intent.getStringExtra("incoming_number");
-				Log.i(TAG, "RINGING :" + incoming_number);
-				break;
-			case TelephonyManager.CALL_STATE_OFFHOOK:
-				if (incomingFlag) {
-					Log.i(TAG, "incoming ACCEPT :" + incoming_number);
+				if (MusicUtil.isPlaying()) {
+					mLastState = MusicState.PLAYING;
+					MusicUtil.pause(context);
 				}
 				break;
-
+			case TelephonyManager.CALL_STATE_OFFHOOK:
+				break;
 			case TelephonyManager.CALL_STATE_IDLE:
-				MusicUtil.play(context);
-				if (incomingFlag) {
-					Log.i(TAG, "incoming IDLE");
+				if (mLastState == MusicState.PLAYING) {
+					MusicUtil.play(context);
 				}
 				break;
 			}
 		}
+	}
+	
+	
+	private static enum MusicState {
+		PAUSED, PLAYING
 	}
 }
