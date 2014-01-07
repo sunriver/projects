909d8b7a17be4d307bc615fe14911ff1416b17ce
sunriver <lu_0047@163.com>
2014-01-07 02:51:00 -0800

    refine handler

--- android/FunnyPlayer/src/com/funnyplayer/util/UncatchExceptionHandler.java
+++ android/FunnyPlayer/src/com/funnyplayer/util/UncatchExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.funnyplayer.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.Thread.UncaughtExceptionHandler;
@@ -8,46 +9,61 @@ import java.util.Date;
 
 import android.content.Context;
 import android.os.Environment;
+import android.text.format.DateFormat;
 import android.util.Log;
 
 public class UncatchExceptionHandler implements Thread.UncaughtExceptionHandler {
 	private final static String TAG = UncatchExceptionHandler.class.getSimpleName();
-	private final static String FILE_NAME = "listen.log";
 	private PrintWriter mLogWriter;
 	private StringBuilder mBuilder = new StringBuilder();
 	private UncaughtExceptionHandler mDefaultHandler;
+	private File mLogFile;
 	
 	public UncatchExceptionHandler(Context context, UncaughtExceptionHandler defaultHandler) {
 		try {
 			mDefaultHandler = defaultHandler;
-			File dir = Environment.getExternalStorageDirectory();
-			File logFile = new File(dir, FILE_NAME);
-			if (!logFile.exists()) {
-				logFile = dir.canWrite() ? dir.createTempFile("listen", "log") : null;
+			String dirPath = Environment.getExternalStorageDirectory().getAbsoluteFile() + "/" + context.getPackageName();
+			File dir = new File(dirPath);
+			if (!dir.exists()) {
+				dir.mkdir();
 			}
-			mLogWriter = logFile != null ?  new PrintWriter(logFile) : null;
+			final String fileName = "crash.txt";
+			mLogFile = new File(dir, fileName);
+			if (!mLogFile.exists()) {
+				mLogFile.createNewFile();
+			}
+	
 		} catch (IOException e) {
-			Log.d(TAG, "Fail to create file listen.log", e);
+			Log.w(TAG, "Fail to create exception log file", e);
 		}
 	}
 
 	@Override
 	public void uncaughtException(Thread thread, Throwable ex) {
 		mDefaultHandler.uncaughtException(thread, ex);
-		if (null == mLogWriter) {
+		if (!mLogFile.exists()) {
 			return;
 		}
-		mBuilder.setLength(0);
-		Date now = new Date();
-		String threadName = thread.getName();
-		long threadId = thread.getId();
-		String errMsg = ex.getMessage();
-		String stackStr = Log.getStackTraceString(ex);
-		mBuilder.append(now.toString())
-				.append(threadId)
-				.append(":").append(threadName)
-				.append(errMsg + "\n" + stackStr);
-		mLogWriter.write(mBuilder.toString());
+		try {
+			mLogWriter = new PrintWriter(mLogFile);
+			mBuilder.setLength(0);
+			Date now = new Date();
+			String threadName = thread.getName();
+			long threadId = thread.getId();
+			String errMsg = ex.getMessage();
+			String stackStr = Log.getStackTraceString(ex);
+			mBuilder.append(now.toString())
+			.append(threadId)
+			.append(":").append(threadName)
+			.append(errMsg + "\n" + stackStr);
+			mLogWriter.write(mBuilder.toString());
+		} catch (FileNotFoundException e) {
+			Log.w(TAG, "fail to catch exception", e);
+		} finally {
+			if (mLogWriter != null) {
+				mLogWriter.close();
+			}
+		}
 	}
 
 }
