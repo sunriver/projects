a608df42b2405124eb9eed0fc6278bd8b902bf1d
sunriver <lu_0047@163.com>
2014-01-26 20:49:02 -0800

    add sliding menu

--- android/CocoReader/src/com/coco/reader/MainActivity.java
+++ android/CocoReader/src/com/coco/reader/MainActivity.java
@@ -1,10 +1,14 @@
 package com.coco.reader;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import com.coco.reader.R;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +16,16 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
+        menu.setMenu(R.layout.menu);
 	}
 
 	@Override
