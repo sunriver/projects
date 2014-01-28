eaf89c31a8bb4afd52ad44f10cafdb83e43d69ea
sunriver <lu_0047@163.com>
2014-01-27 20:51:01 -0800

    change "ReadView" to "PageView"

--- android/CocoReader/src/com/coco/reader/view/PageView.java
+++ android/CocoReader/src/com/coco/reader/view/PageView.java
@@ -1,22 +1,23 @@
 package com.coco.reader.view;
 
 
+import com.aphidmobile.flip.FlipViewController;
 import com.codo.reader.data.Document;
 import com.sunriver.common.utils.ApiUtil;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.ScrollingMovementMethod;
 import android.util.AttributeSet;
 import android.widget.EditText;
 
 public class ReadView extends EditText {
 	private final static String TAG = ReadView.class.getSimpleName();
-	private Document mDocument;
-
 	
 	public ReadView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		mDocument = new Document(context);
 		init(context);
 	}
 
@@ -26,6 +27,32 @@ public class ReadView extends EditText {
 	}
 	
 	private void init(final Context context) {
+//		this.setMovementMethod(ScrollingMovementMethod.getInstance());
+//		this.setSelection(getText().length(), getText().length());
+//		this.addTextChangedListener(new TextWatcher() {
+//
+//			@Override
+//			public void beforeTextChanged(CharSequence s, int start, int count,
+//					int after) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void onTextChanged(CharSequence s, int start, int before,
+//					int count) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void afterTextChanged(Editable s) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//			
+//		});
+		
 		Handler handler = new Handler();
 		handler.post(new Runnable() {
 			@Override
@@ -44,9 +71,6 @@ public class ReadView extends EditText {
 
 		@Override
 		protected String doInBackground(Void... params) {
-			if (mDocument != null) {
-				return mDocument.nextPage();
-			}
 			return null;
 		}
 
