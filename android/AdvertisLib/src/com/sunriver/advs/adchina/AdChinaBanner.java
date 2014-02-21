fcaf9a901378ce8418600e2159b755fd0df84636
sunriver <lu_0047@163.com>
2014-02-21 00:26:53 -0800

    refactor

--- android/AdvertisLib/src/com/sunriver/advs/adchina/AdChinaBanner.java
+++ android/AdvertisLib/src/com/sunriver/advs/adchina/AdChinaBanner.java
@@ -2,17 +2,20 @@ package com.sunriver.advs.adchina;
 
 
 import com.adchina.android.ads.AdManager;
+import com.adchina.android.ads.api.AdBannerListener;
 import com.adchina.android.ads.api.AdView;
 
 import android.support.v4.app.Fragment;
 import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-public class AdChinaBanner extends Fragment  {
+public class AdChinaBanner extends Fragment implements AdBannerListener {
 	private static final String TAG = AdChinaBanner.class.getSimpleName();
 	
 	private static final String AD_ID = "2134693";
@@ -30,12 +33,39 @@ public class AdChinaBanner extends Fragment  {
 		Context ctx = getActivity().getApplicationContext();
 
 		AdView view = new AdView(ctx, AD_ID, true, true);
+		view.setAdBannerListener(this);
 //		view.setAdReferenceSize(480, 72);
 		parent.addView(view);
 
 		view.setAdRefreshTime(20);
 		AdManager.setRelateScreenRotate(ctx, true);
+		AdManager.setCanHardWare(true);
+		AdManager.setEnableLbs(true); 
+		AdManager.setAnimation(true);
+		LocationManager locationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+		AdManager.setLocationManager(locationManager);
 		return parent;
+	}
+
+
+
+	@Override
+	public void onClickBanner(AdView v) {
+		Log.d(TAG, "onClickBanner()+");
+	}
+
+
+
+	@Override
+	public void onFailedToReceiveAd(AdView v) {
+		Log.d(TAG, "onFailedToReceiveAd()+");
+	}
+
+
+
+	@Override
+	public void onReceiveAd(AdView v) {
+		Log.d(TAG, "onReceiveAd()+");
 	}
 
 }
