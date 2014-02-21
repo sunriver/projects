da74355068fd2d779f225f32c52d6d03e97bdef5
sunriver <lu_0047@163.com>
2014-02-21 00:07:50 -0800

    refactor

---
+++ android/AdvertisLib/src/com/sunriver/advs/adchina/AdChinaBanner.java
@@ -0,0 +1,41 @@
+package com.sunriver.advs.adchina;
+
+
+import com.adchina.android.ads.AdManager;
+import com.adchina.android.ads.api.AdView;
+
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class AdChinaBanner extends Fragment  {
+	private static final String TAG = AdChinaBanner.class.getSimpleName();
+	
+	private static final String AD_ID = "2134693";
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return createAdView();
+	}
+	
+	
+	
+	private LinearLayout createAdView() {
+		LinearLayout parent = new LinearLayout(getActivity());
+		Context ctx = getActivity().getApplicationContext();
+
+		AdView view = new AdView(ctx, AD_ID, true, true);
+//		view.setAdReferenceSize(480, 72);
+		parent.addView(view);
+
+		view.setAdRefreshTime(20);
+		AdManager.setRelateScreenRotate(ctx, true);
+		return parent;
+	}
+
+}
---
+++ android/AdvertisLib/src/com/sunriver/advs/adwo/AdwoBanner.java
@@ -0,0 +1,49 @@
+package com.sunriver.advs.adwo;
+
+import com.adwo.adsdk.AdListener;
+import com.adwo.adsdk.AdwoAdView;
+import com.adwo.adsdk.ErrorCode;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.RelativeLayout;
+
+public class AdwoBanner extends Fragment implements AdListener {
+	private static final String TAG = AdwoBanner.class.getSimpleName();
+	private static final boolean AD_TEST_MODE = false;
+	private static final String Adwo_PID = "70e074f4698745ab9f349becac751441";
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return createAdView();
+	}
+	
+
+	private RelativeLayout createAdView() {
+		RelativeLayout paprent = new RelativeLayout(getActivity());
+		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		lp.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		AdwoAdView.setBannerMatchScreenWidth(true);
+		AdwoAdView adView = new AdwoAdView(getActivity(), Adwo_PID, AD_TEST_MODE, 30);
+		adView.setListener(this);
+		paprent.addView(adView, lp);
+		return paprent;
+	}
+
+
+	@Override
+	public void onFailedToReceiveAd(AdwoAdView arg0, ErrorCode arg1) {
+		Log.e(TAG, "onFailedToReceiveAd() errorCode:" + arg1.getErrorCode());
+	}
+
+
+	@Override
+	public void onReceiveAd(AdwoAdView arg0) {
+		Log.v(TAG, "onReceiveAd()+");
+	}
+}
