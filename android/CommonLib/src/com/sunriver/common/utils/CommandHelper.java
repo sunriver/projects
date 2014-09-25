c17af0c2f08a85b4181def7c7e6eda5c0c75a264
sunriver <lu_0047@163.com>
2014-09-25 15:48:31 +0800

    gain root permission

--- android/CommonLib/src/com/sunriver/common/utils/CommandHelper.java
+++ android/CommonLib/src/com/sunriver/common/utils/CommandHelper.java
@@ -1,5 +1,6 @@
 package com.sunriver.common.utils;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -39,5 +40,56 @@ public class CommandHelper {
 	
 	public static String runCmdID() {
 		return runCmd("id");
+	}
+	
+	public static boolean hasRootPermission() {
+		Process process = null;
+		DataOutputStream os = null;
+		boolean rooted = true;
+		try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+			if (process.exitValue() != 0) {
+				rooted = false;
+			}
+		} catch (Exception e) {
+			rooted = false;
+		} finally {
+			if (os != null) {
+				try {
+					os.close();
+					process.destroy();
+				} catch (Exception e) {
+				}
+			}
+		}
+		return rooted;
+	}
+
+	public static boolean runRootCommand(String command) {
+		Process process = null;
+		DataOutputStream os = null;
+		try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(command + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+		} catch (Exception e) {
+			return false;
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+				process.destroy();
+			} catch (Exception e) {
+			}
+		}
+		return true;
 	}
 }
